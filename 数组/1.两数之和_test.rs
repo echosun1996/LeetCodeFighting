00cbb56cbf01ccace777bd693484982ecff5b527
winkyle <hyq@spacetech.com.cn>
2020-12-26 17:24:38 +0800

    注释

--- 数组/1.两数之和_test.rs
+++ 数组/1.两数之和_test.rs
@@ -1,27 +1,26 @@
+/// 解法一
+/// 暴力破解，直接两层for循环数组，得到相加数等于目标值的加数的下标
 //pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 //    let mut i: i32 = 0;
 //    let mut j: i32 = 0;
-//    let mut flag: bool = false;
 //    let len: i32 = nums.len() as i32;
 //    while i < (len - 1) {
 //        j = i + 1;
 //        while j < len {
 //            if nums[i.clone() as usize] + nums[j as usize] == target {
-//                flag = true;
-//                break;
+//               return vec![i, j];
 //            }
 //            j = j.clone() + 1;
 //        }
-//        if flag {
-//            break;
-//        }
-//
 //        i = i.clone() + 1;
 //    }
 //    vec![i, j]
 //}
-///
+
+
 /// 解法2
+/// 一层循环，用map存储其中一个加数和它的下标(循环数组时，map中没有找到与当前加数相加符合结果值的另一个加数，则将当前加数存进map)，
+/// 循环一次数组得到相加数为目标值的map中加数与数组中加数的下标
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32>{
     let mut i:i32 = 0;
     let len:i32=nums.len() as i32;
